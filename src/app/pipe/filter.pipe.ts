c8aae4f6abbf1ead84736ae740cb1886fbfc1af0
danielwine <danielwine@tutamail.com>
2022-01-21 18:02:32 +0100

    Improve filterpipe and product-list. Update cat01 page.

--- src/app/pipe/filter.pipe.ts
+++ src/app/pipe/filter.pipe.ts
@@ -5,13 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
   transform(value: any[], phrase: string, key: string = ''): any {
-    if (!Array.isArray(value) || !phrase) return value;
-    return value.filter((item) => {
-      return (item.author.toString() + ' ' + item.title.toString())
-        .toLowerCase()
-        .includes(phrase.toLowerCase())
-        ? true
-        : false;
-    });
+    if (!Array.isArray(value) || !phrase || !key) return value
+    return value.filter(
+      item => {
+        return item[key]
+          .toString().toLowerCase()
+          .includes(phrase.toLowerCase()) ? true : false
+      }
+    )
   }
 }
