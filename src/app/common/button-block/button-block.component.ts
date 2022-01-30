56425e8d372ce79e7e24f6ce18d837f503e9688f
danielwine <danielwine@tutamail.com>
2022-01-30 22:59:58 +0100

    Add filter & bootstrap classes & button methods.

--- src/app/common/button-block/button-block.component.ts
+++ src/app/common/button-block/button-block.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../../service/product.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product';
 
 @Component({
   selector: 'app-button-block',
   templateUrl: './button-block.component.html',
-  styleUrls: ['./button-block.component.scss']
+  styleUrls: ['./button-block.component.scss'],
 })
 export class ButtonBlockComponent implements OnInit {
+  @Input() product: Product = new Product();
+  constructor(private productService: ProductService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSave(product: Product) {
+    this.productService.updateOne(this.product);
   }
 
+  onDelete(product: Product) {
+    this.productService.deleteOne(this.product);
+  }
+
+  ngOnInit(): void {}
 }
